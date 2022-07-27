725b265325e2fc3dd7af16d70d8054596a092f72
edward93 <edward.chopuryan@gmail.com>
2022-07-27 12:45:06 -0400

    adds more logging in api

--- api/imgEditor.ts
+++ api/imgEditor.ts
@@ -4,12 +4,14 @@ import { addFrame } from "@edward1993/img-editor/dist/lib/imgEditor";
 
 // how to receive a file https://github.com/vercel/micro/issues/434
 const handle = async (request: VercelRequest, response: VercelResponse) => {
-  console.log(request);
+  console.log("Request - ", request);
   if (Buffer.isBuffer(request.body)) {
     const file = "./uploaded-file.png";
     fs.createWriteStream(file).write(request.body);
     const result = await addFrame([file]);
     response.status(200).send(`DONE - ${result}`);
+  } else {
+    response.status(500).send(`body wasn't a buffer`);
   }
 };
 
