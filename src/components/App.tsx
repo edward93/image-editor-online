cad0b10aba7d0de0a65d86eff54af2f41723de6c
edward93 <edward.chopuryan@gmail.com>
2022-08-08 15:11:16 -0400

    adds new ga 4 and remove old npm package

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,9 @@
-import React, { useEffect } from "react";
-import ReactGA from "react-ga";
-
 import Header from "./Header.Component";
 import ImageEditor from "./Image.Editor.Component";
 
 import Footer from "./Footer.Component";
 
-ReactGA.initialize("G-FHPMY22TTL", {
-  debug: true,
-  titleCase: false,
-});
-
 function App() {
-  useEffect(() => {
-    ReactGA.ga('set', 'checkProtocolTask', null);
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <div className="ieo-app-content">
       <Header />
